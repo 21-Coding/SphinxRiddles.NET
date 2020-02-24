4b5db22f4f6a12ed2debc826a0b3aee1f20b6a36
Student <example@example.com>
2020-02-24 10:39:53 -0800

    add three working riddles

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ namespace sphinxApp
     {
         static void riddleCall(string riddles)
         {
-        
-          
+           
+        if (true)
+          {
               Console.WriteLine("Is the " + riddles);
-          
+          }
+            
           
         }
         static void Main()
@@ -22,20 +24,45 @@ namespace sphinxApp
             {
                 riddleCall("sky blue?");
                 string userInput0 = Console.ReadLine();
-               
-            }
-             else if (riddleAnswer == true)
-            {
+
+                if (riddleAnswer == true)
+
+                {
                 riddleCall("grass green?");
                 string userInput1 = Console.ReadLine();
+                }
+               
             }
-            else if (riddleAnswer == true)
-            {
-                riddleCall("lemonade yellow?");
-                string userInput2 = Console.ReadLine();
-            }
+            
            
-                
         }
     }
 }
+
+
+
+
+
+            // riddleCall("road smooth?");
+            // string userInput = Console.ReadLine();
+            // bool riddleAnswer =  bool.Parse(userInput);
+
+            // if (riddleAnswer == true)
+            // {
+            //     riddleCall("sky blue?");
+            //     string userInput0 = Console.ReadLine();
+               
+            // }
+            //  else if (riddleAnswer == true)
+            // {
+            //     riddleCall("grass green?");
+            //     string userInput1 = Console.ReadLine();
+            // }
+            // else  
+            // {
+            //     riddleCall("lemonade yellow?");
+            //     string userInput2 = Console.ReadLine();
+            
+           
+             
+
