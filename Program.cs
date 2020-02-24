adeed67f25f4f85b85fe6ef67bdf0cfca8e908bd
Student <example@example.com>
2020-02-24 09:48:01 -0800

    add sphinx riddle code

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace sphinxApp
+{
+    class Program
+    {
+        static void riddleCall(string riddles)
+        {
+        
+          
+              Console.WriteLine("Is the " + riddles);
+          
+          
+        }
+        static void Main()
+        {
+            riddleCall("road smooth?");
+            string userInput = Console.ReadLine();
+            bool riddleAnswer =  bool.Parse(userInput);
+
+            if (riddleAnswer == true)
+            {
+                riddleCall("sky blue?");
+                string userInput0 = Console.ReadLine();
+               
+            }
+             else if (riddleAnswer == true)
+            {
+                riddleCall("grass green?");
+                string userInput1 = Console.ReadLine();
+            }
+            else if (riddleAnswer == true)
+            {
+                riddleCall("lemonade yellow?");
+                string userInput2 = Console.ReadLine();
+            }
+           
+                
+        }
+    }
+}
